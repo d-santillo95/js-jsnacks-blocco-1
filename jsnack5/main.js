05bad6c1bf70fb73fdc93735f1350895fc8ca65e
d-santillo95 <daniele.santillo95@gmail.com>
2020-04-23 15:35:41 +0100

    esercizio completo

---
+++ jsnack5/main.js
@@ -0,0 +1,5 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
+var n = parseInt(prompt('inserisci un numero'));
+for (var i = 0; i <= n; i++) {
+    console.log(i * i * i);
+}
---
+++ jsnack6/main.js
@@ -0,0 +1,6 @@
+//Stampa le potenze di 2 fino a 1000
+var n = 1;
+for (var i = 0; n < 1000; i++) {
+    console.log(n);
+    n = n * 2;
+}
